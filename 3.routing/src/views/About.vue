ceca2570a77fbae2f540a17a0f1886f5192a1e2e
ceciliecastor <ceciliecastor@gmail.com>
2024-09-24 12:39:02 +0200

    Router-link setup Not found Listing and details side

---
+++ 3.routing/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis optio nisi at vitae molestias obcaecati, iure esse quibusdam. Ipsum explicabo suscipit et, aut nam sint maiores voluptatum deleniti aliquam esse minus assumenda nisi perspiciatis molestiae quae sed inventore amet nobis eaque laudantium incidunt! Recusandae accusamus laboriosam aspernatur suscipit. Commodi quae perferendis, tempore inventore aliquam veritatis! Amet quam accusamus odit alias ducimus harum culpa iste hic obcaecati quod! Aliquid veritatis accusantium dolorem fugiat. Harum a neque repellat doloribus repellendus rerum eaque?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum eligendi minus quas error corrupti quia facilis! Aliquam, id, vero error provident consectetur perspiciatis fuga, vitae accusantium doloremque eveniet sunt rem quae laboriosam ex est odit. Ab magnam sapiente est quos! Repudiandae reiciendis harum tempora, vel modi, incidunt, inventore odio nihil quaerat officia officiis. Itaque odio fuga officia? Voluptatibus corporis rerum eligendi vitae eum ut enim. Quidem architecto assumenda ut voluptatem numquam dolorum animi voluptatibus, saepe soluta repellendus. Doloribus, dicta. Provident, blanditiis! Et totam, eligendi aut repellendus accusamus earum cupiditate itaque quidem modi beatae veritatis placeat nostrum suscipit illum consectetur aliquam? Illum veniam sapiente dolore odit minima velit accusamus dolores ipsum.
+    </p>
+  </div>
+</template>
---
+++ 3.routing/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis inventore nam neque deserunt ipsam, ullam similique exercitationem placeat iusto, corrupti quasi libero, voluptas dolores cumque. Nam eum enim veritatis repellat iusto porro consequuntur a quam rerum hic molestiae deserunt omnis neque, natus aliquid dolores voluptas tempore! Dolore soluta illo eos, similique incidunt temporibus ipsam dolores blanditiis officiis modi magnam. Incidunt nostrum quis hic aliquam mollitia asperiores neque optio magnam. Sed aut voluptate voluptas dolorum porro ex iste nihil a qui, dolore molestias quae quos deleniti delectus? Numquam dignissimos est at deleniti nulla sapiente illum? Rerum delectus consequatur excepturi quos labore.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis inventore nam neque deserunt ipsam, ullam similique exercitationem placeat iusto, corrupti quasi libero, voluptas dolores cumque. Nam eum enim veritatis repellat iusto porro consequuntur a quam rerum hic molestiae deserunt omnis neque, natus aliquid dolores voluptas tempore! Dolore soluta illo eos, similique incidunt temporibus ipsam dolores blanditiis officiis modi magnam. Incidunt nostrum quis hic aliquam mollitia asperiores neque optio magnam. Sed aut voluptate voluptas dolorum porro ex iste nihil a qui, dolore molestias quae quos deleniti delectus? Numquam dignissimos est at deleniti nulla sapiente illum? Rerum delectus consequatur excepturi quos labore.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
