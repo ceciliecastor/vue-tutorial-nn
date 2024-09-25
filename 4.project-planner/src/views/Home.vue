b452240db6be2dd744fd5e87492e1e631f42268e
ceciliecastor <ceciliecastor@gmail.com>
2024-09-25 12:37:50 +0200

    showDetails toggle delete function

--- 4.project-planner/src/views/Home.vue
+++ 4.project-planner/src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        {{ project.title }}
+        <SingleProject :project="project" @delete="handleDelete" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "@/components/SingleProject.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
@@ -22,6 +24,13 @@ export default {
       .then((res) => res.json())
       .then((data) => (this.projects = data))
       .catch((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
   },
 };
 </script>
