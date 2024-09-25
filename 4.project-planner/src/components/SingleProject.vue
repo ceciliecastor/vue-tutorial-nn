492c164ce31af2f2bff5550ed5c84658076037d7
ceciliecastor <ceciliecastor@gmail.com>
2024-09-25 12:57:23 +0200

    Complete button and func done

--- 4.project-planner/src/components/SingleProject.vue
+++ 4.project-planner/src/components/SingleProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="toggleDetails">
         {{ project.title }}
@@ -7,7 +7,7 @@
       <div class="icons">
         <span class="material-icons">edit</span>
         <span class="material-icons" @click="deleteProject">delete</span>
-        <span class="material-icons">done</span>
+        <span class="material-icons tick" @click="toggleComplete">done</span>
       </div>
     </div>
     <div class="details" v-if="showDetails">
@@ -33,6 +33,17 @@ export default {
       //delete request takes a secon argument: an objects object
       fetch(this.uri, { method: "DELETE" })
         .then(() => this.$emit("delete", this.project.id))
+        .catch((err) => console.log(err));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => {
+          this.$emit("complete", this.project.id);
+        })
         .catch((err) => console.log(err));
     },
   },
@@ -63,5 +74,11 @@ h3 {
 }
 .material-icons:hover {
   color: #777;
+}
+.project.complete {
+  border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+  color: #00ce89;
 }
 </style>
